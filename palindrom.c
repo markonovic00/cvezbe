5a10544dd7924ccfec41cca1309d09867341be63
markonovic00 <markonovic00@gmail.com>
2018-11-16 15:17:54 +0100

    Polindromi

---
+++ palindrom.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	char str1[101];
+	printf("Unesite string : ");
+	fgets(str1,101,stdin);
+	
+	printf("\n%s\n",str1);
+	
+	int i;
+	int duzina = strlen(str1);
+	
+  	int flag=0;
+	printf("%d\n",duzina);
+  	
+  	for(i=0;i<duzina;i++){
+  	
+  		if(isalpha(str1[i]) && str1[i] !=' '){
+  			str1[i]=tolower(str1[i]);
+  			printf("str[%d]=%c | str[%d]=%c\n",i,str1[i],duzina-2-i,str1[duzina-2-i]);
+  			if(str1[i]==str1[duzina-i-2]){
+  				flag=1;
+  			}
+  			else if(str1[i]!=str1[duzina-i-2]){
+  				flag=0; 			
+  			}
+  		}
+  	}
+  	
+  	
+	if(flag==1){
+		printf("\n%s je polindrom",str1);
+	}
+	else{
+		printf("Nije polindrom");
+	}
+
+	return 0;
+}
--- stringovizadatak.c
+++ stringovizadatak.c
@@ -3,7 +3,7 @@
 
 int main(){
 	
-	char str1[101];
+	char str1[101],*str2;
 	
 	int duzina;
 	
@@ -15,8 +15,17 @@ int main(){
 	for(i=duzina;i>=0;i--){
 		printf("%c",str1[i]);
 	}
+	
 	printf("\n");
-
+	
+	int brojac=0;
+	str2=str1;	
+	while(*str2){
+		brojac++;	
+		str2++;
+	}
+	
+	printf("brojac %d\n",brojac-1);
 	
 	return 0;
 }
